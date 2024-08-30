90142ea21a309e49c844c150a917203b0aee4050
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-08-30 20:03:41 +0200

    UseReducerConcept

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-import Greet from './greet';
-import ParentComponent from './ParentComponent';
-import Welcome from './welcome';
-import UseStateArray from './UseStateArray';
-import UseEffectFetch from './UseEffectFetch';
+// import Greet from './greet';
+// import ParentComponent from './ParentComponent';
+// import Welcome from './welcome';
+// import UseStateArray from './UseStateArray';
+// import UseEffectFetch from './UseEffectFetch';
+// import UseReducerSimple from './UseReducerSimple';
+import UseReducerObject from './UseReducerObject';
 
 function App() {
   return (
     <div className="App">
-      <Greet firstname="John" lastname="Doe" />
+      {/* <Greet firstname="John" lastname="Doe" />
       <Welcome firstname="John" lastname="Doe" />
       <ParentComponent />
       <UseStateArray />
-      <UseEffectFetch />
+      <UseReducerSimple /> */}
+      <UseReducerObject />
+      {/* <UseEffectFetch /> */}
     </div>
   );
 }
